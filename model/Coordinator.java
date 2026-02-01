6e65605de44c6e1c2e123a8506f87288613904e9
Vipul Kumar <vipulkumar01005@gmail.com>
2026-02-01 17:55:44 +0800

    Delete model directory

--- model/Coordinator.java
+++
@@ -1,6 +0,0 @@
-package model;
-public class Coordinator extends User {
-    public Coordinator(String id, String name, String email, String pass) {
-        super(id, name, email, pass, "Coordinator");
-    }
-}
